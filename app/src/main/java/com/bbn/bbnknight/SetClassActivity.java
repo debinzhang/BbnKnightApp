49dd6ab891fc4160d7cd14d31413d016701e2b08
dzhang <debin.zhang@viasat.com>
2019-12-29 17:19:48 -0500

    Add class setting screen

--- app/src/main/java/com/bbn/bbnknight/SetClassActivity.java
+++ app/src/main/java/com/bbn/bbnknight/SetClassActivity.java
@@ -1,14 +1,95 @@
 package com.bbn.bbnknight;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SetClassActivity extends AppCompatActivity {
+    public class ClassItem {
+        public String name;
+        public String block;
+    };
+
+    ListView mListView;
+    public ArrayList<ClassItem> mClasses = new ArrayList<>();
+    ClassListAdaptor mClassListAdaptor;
+
+    public void initClassBlocks() {
+        ClassItem classItem;
+        classItem = new ClassItem();
+        classItem.name = "Math";
+        classItem.block = "A Block";
+        mClasses.add(classItem);
+
+        classItem = new ClassItem();
+        classItem.name = "English";
+        classItem.block = "C Block";
+        mClasses.add(classItem);
+
+        classItem = new ClassItem();
+        classItem.name = "Chemistry";
+        classItem.block = "D Block";
+        mClasses.add(classItem);
+
+        classItem = new ClassItem();
+        classItem.name = "Computer Science";
+        classItem.block = "G Block";
+        mClasses.add(classItem);
+
+        classItem = new ClassItem();
+        classItem.name = "Physics";
+        classItem.block = "H Block";
+        mClasses.add(classItem);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_class);
+
+        initClassBlocks();
+
+        mListView = findViewById(R.id.classListView);
+        mClassListAdaptor = new ClassListAdaptor(this, android.R.layout.simple_list_item_1,
+                mClasses);
+        mListView.setAdapter(mClassListAdaptor);
+    }
+
+    class ClassListAdaptor extends ArrayAdapter<ClassItem> {
+        public ClassListAdaptor(@NonNull Context context, int resource, ArrayList<ClassItem> classList) {
+            super(context, resource, classList);
+        }
+
+        @Override
+        public int getCount() {
+            return mClasses.size();
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.class_item_layout, null);
+
+            TextView classNameTextView = view.findViewById(R.id.classNameTextView);
+            TextView classBlockTextView = view.findViewById(R.id.classBlockTextView);
+
+            classNameTextView.setText(mClasses.get(position).name);
+            classBlockTextView.setText(mClasses.get(position).block);
+
+            return view;
+        }
     }
 }
