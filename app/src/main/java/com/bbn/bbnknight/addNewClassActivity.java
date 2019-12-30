a77b1e26118f43de3893330faf3fcf2d02caef86
dzhang <debin.zhang@viasat.com>
2019-12-30 00:58:34 -0500

    Add color picker

--- app/src/main/java/com/bbn/bbnknight/addNewClassActivity.java
+++ app/src/main/java/com/bbn/bbnknight/addNewClassActivity.java
@@ -1,14 +1,44 @@
 package com.bbn.bbnknight;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class addNewClassActivity extends AppCompatActivity {
+    int mColor;
+    TextView mClassNameEditText;
+    Button mPickColorButton;
+
+    // pick color button clicked
+    public void openColorPicker(View view) {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, mColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mColor = color;
+                mClassNameEditText.setTextColor(mColor);
+                mPickColorButton.setBackgroundColor(mColor);
+            }
+        });
+        colorPicker.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_class);
+
+        mColor  = ContextCompat.getColor(this, R.color.colorPrimary);
+        mClassNameEditText = findViewById(R.id.classNameEditText);
+        mPickColorButton = findViewById(R.id.pickColorButton);
     }
 }
